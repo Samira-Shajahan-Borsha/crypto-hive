9ffa7e5a521960df0add93db514beaabea54132b
Samira Shajahan Borsha <57401425+Samira-Shajahan-Borsha@users.noreply.github.com>
2023-02-16 05:25:21 +0600

    styled header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,11 +3,11 @@ import { NavLink } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <nav>
-            <div className=''>
-                <NavLink to='/'>Crypto Hive</NavLink>
+        <nav className='sticky top-0 flex justify-center md:justify-between px-12 py-8 bg-white'>
+            <div className='hidden md:block'>
+                <NavLink to='/' className='text-xl text-gray-600 font-sans font-bold'>Crypto Hive</NavLink>
             </div>
-            <div>
+            <div className='font-sans flex text-xl gap-4'>
                 <NavLink to='/'>Home</NavLink>
                 <NavLink to='/coins'>Coins</NavLink>
                 <NavLink to='/contact'>Contact</NavLink>
