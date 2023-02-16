1b263f918f13b365eef61f2a46e476c4b5a59e42
Samira Shajahan Borsha <57401425+Samira-Shajahan-Borsha@users.noreply.github.com>
2023-02-16 17:51:16 +0600

    styled home page

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const HomePage = () => {
     return (
-        <div>
-            <h1>HomePage</h1>
+        <div className='flex flex-col bg-gray-100 h-[80vh] justify-center items-center '>
+            <h1 className='font-mono font-medium md:text-5xl text-2xl mb-3 sm:px-4'>Welcome to CRYPTO Hive</h1>
+            <Link to='/coins'>
+                <button className='bg-cyan-600 font-normal rounded-full  py-2 px-3 text-white'>Explore coins</button>
+            </Link>
         </div>
     );
 };
